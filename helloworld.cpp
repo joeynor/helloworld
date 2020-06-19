9584e4df8c541372b5a45933bcc675df34d2f23f
Rizal Mohd Nor <joey.aka.rizal@gmail.com>
2020-06-20 06:51:04 +0800

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+// Hello World Starter Code
+// Name: <make sure to put your name here
+// Matric ID: <make sure to have your matric ID here, failure will result in 0 grades 
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+ // put your code here
+}
